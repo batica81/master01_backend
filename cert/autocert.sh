1bf30e7ac6923fa322528bf00d22b2995358ecab
voja <batica@gmail.com>
2018-03-31 14:49:57 +0200

    cert

---
+++ cert/autocert.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+printf '\n'
+printf 'Generisanje klijentskog sertifikata:\n'
+printf '\n'
+openssl req -new -passout pass:$2 -config client.conf -out certs/$1.csr -keyout certs/$1.key 
+
+printf '\n'
+printf 'Potpisivanje klijentskog sertifikata:\n'
+printf '\n'
+openssl ca -config ca.conf -batch -in certs/$1.csr -out certs/$1.crt -extensions client_ext
+
+printf '\n'
+printf 'Izvoz klijentskog sertifikata u p12 format:\n'
+printf '\n'
+openssl pkcs12 -export -clcerts -in certs/$1.crt -inkey certs/$1.key -out certs/$1.p12
+
+printf '\n'
+printf 'Izvoz klijentskog sertifikata u pem format:\n'
+printf '\n'
+openssl pkcs12 -in certs/$1.p12 -out certs/$1.pem -clcerts
+
+printf '\n'
+printf 'Prikaz hash vrednosti klijentskog sertifikata:\n'
+printf '\n'
+openssl x509 -noout -fingerprint -sha1 -inform pem -in certs/$1.pem
